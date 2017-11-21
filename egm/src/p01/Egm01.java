1f9d9d58fcac77a405d1636dd3d05c8dfe136348
kimjeongkyu <heykkyu@gmail.com>
2017-11-21 18:31:48 +0900

    project init

---
+++ egm/src/p01/Egm01.java
@@ -0,0 +1,15 @@
+package p01;
+
+public class Egm01 {
+	
+
+public static void main(String[] args) {
+int a = 3;
+int b = 4;
+System.out.println(a*b);
+System.out.println(a+b);
+System.out.println(a-b);
+System.out.println(a/b);
+System.out.println(a%b);
+}
+}
